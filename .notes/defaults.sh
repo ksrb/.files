a370bc6099e941dd86bdd492e3a2125f6bddf6ed
Kevin Suen <ksrbkevinsuen@gmail.com>
2015-09-26 12:11:01 -0700

    New script to set Mac defaults - Environments.md   - Modified to reference script   - Additional notes on various system settings add to

---
+++ .notes/defaults.sh
@@ -0,0 +1,23 @@
+set(){
+  # Remove mouse acceleration
+  defaults write .GlobalPreferences com.apple.mouse.scaling -1
+  # Show full path in finder
+  defaults write com.apple.finder _FXShowPosixPathInTitle -bool YES
+  # Remove hide/show animation
+  defaults write com.apple.dock autohide-time-modifier -int 0 && killall Dock
+  # Disable dashboard/widgets/F12
+  defaults write com.apple.dashboard mcx-disabled -boolean YES && killall Dock
+  # Disable window animation
+  defaults write -g NSAutomaticWindowAnimationsEnabled -bool false
+}
+
+# TODO
+reset(){
+}
+
+addSpacerToDock(){
+  # Add spacer Dock
+  defaults write com.apple.dock persistent-apps -array-add '{tile-data={}; tile-type="spacer-tile";}'; killall Dock
+}
+
+"$@"
