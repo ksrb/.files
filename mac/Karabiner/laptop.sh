21e7d4467e299031b000e93e03a60b8588fc66ec
Kevin Suen <ksrbkevinsuen@gmail.com>
2016-05-30 23:49:00 -0700

    Mac: Karabiner updated profiles with changes and for new karabiner version

--- mac/Karabiner/laptop.sh
+++ mac/Karabiner/laptop.sh
@@ -2,34 +2,38 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
-$cli set repeat.initial_wait 400
-/bin/echo -n .
-$cli set passthrough.escapetab 1
-/bin/echo -n .
 $cli set custom.mediaKeys2FnKeys 1
 /bin/echo -n .
-$cli set laptop.OptionRQ2Command_RQ 1
+$cli set laptop.cmdShiftW2CmdWOnlyHipChat 1
 /bin/echo -n .
-$cli set laptop.ctrlL2commandRexceptTerminalAndIntelliJ 1
-/bin/echo -n .
-$cli set remap.reverse_both_scrolling 1
-/bin/echo -n .
-$cli set laptop.OptionRTab2Command_RTab 1
-/bin/echo -n .
-$cli set repeat.wait 15
-/bin/echo -n .
-$cli set laptop.controlLOptionR2CommandLOptionRqweasdzxcOnlyTerminalAndIntelliJ 1
-/bin/echo -n .
-$cli set laptop.controlRSpace2CommandRSpace 1
+$cli set laptop.cmdWOnlyHipChat2cmdShiftW 1
 /bin/echo -n .
 $cli set laptop.command_RSpace2Esc 1
 /bin/echo -n .
 $cli set laptop.commandL2OptionR 1
 /bin/echo -n .
-$cli set laptop.OptionRBackquote2Command_RBackquote 1
+$cli set laptop.controlLOptionR2CommandLOptionRqweasdzxcOnlyTerminalAndIntelliJ 1
+/bin/echo -n .
+$cli set laptop.controlRSpace2CommandRSpace 1
+/bin/echo -n .
+$cli set laptop.ctrlL2commandRexceptTerminalAndIntelliJ 1
 /bin/echo -n .
 $cli set laptop.OptionL2Control_RExpTerminalAndIntelliJ 1
 /bin/echo -n .
 $cli set laptop.OptionL2Control_ROnlyTerminalAndINtelliJ 1
+/bin/echo -n .
+$cli set laptop.OptionRBackquote2Command_RBackquote 1
+/bin/echo -n .
+$cli set laptop.OptionRQ2Command_RQ 1
+/bin/echo -n .
+$cli set laptop.OptionRTab2Command_RTab 1
+/bin/echo -n .
+$cli set passthrough.escapetab 1
+/bin/echo -n .
+$cli set remap.reverse_both_scrolling 1
+/bin/echo -n .
+$cli set repeat.initial_wait 350
+/bin/echo -n .
+$cli set repeat.wait 15
 /bin/echo -n .
 /bin/echo
--- mac/Karabiner/legacy.sh
+++ mac/Karabiner/legacy.sh
@@ -2,20 +2,20 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
+$cli set ekeyboard.commandL2controlL_excustomExpTerm 1
+/bin/echo -n .
 $cli set ekeyboard.controlL2commandL_excustomExpTerm 1
-/bin/echo -n .
-$cli set repeat.wait 15
-/bin/echo -n .
-$cli set repeat.initial_wait 400
 /bin/echo -n .
 $cli set ekeyboard.ctrlLSpace2commandLSpace 1
 /bin/echo -n .
 $cli set passthrough.escapetab 1
 /bin/echo -n .
-$cli set ekeyboard.commandL2controlL_excustomExpTerm 1
-/bin/echo -n .
 $cli set remap.commandSpace2commandLshiftLspace 1
 /bin/echo -n .
 $cli set remap.reverse_both_scrolling 1
+/bin/echo -n .
+$cli set repeat.initial_wait 400
+/bin/echo -n .
+$cli set repeat.wait 15
 /bin/echo -n .
 /bin/echo
